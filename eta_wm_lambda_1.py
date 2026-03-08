ae3ada022a0ba1de1d7afa3386cb2b68096a716a
aleber86 <alexisgberlot@gmail.com>
2026-03-08 08:15:28 -0300

    Re-ordered opencl kernels. Moved to src. Implemented a new 'digest' function for eta_wm_lambda_1.py

--- eta_wm_lambda_1.py
+++ eta_wm_lambda_1.py
@@ -10,25 +10,49 @@ _pi = 4.0*np.arctan(1.0) # System definition of pi
 np.random.seed(_random_seed)
 
 class Experiment_execution_using_file(Experiment_execution):
-    def __init__(self,output_file : str, arguments_of_the_map : dict, input_file : str):
+    def __init__(self,output_file : str, arguments_of_the_map : dict):
         Experiment_execution.__init__(self, output_file, arguments_of_the_map)
-        self.input_file_path = input_file
 
-    def set_file_as_initial_conditions(self):
-        with open(self.input_file_path, "r") as file_input:
+    def set_file_as_initial_conditions(self, input_file : str ):
+        with open(input_file, "r") as file_input:
             array_input = np.loadtxt(file_input, dtype = _wp)
             self._lambda_1 = array_input[:,0].copy()
             self._lambda_2 = array_input[:,1].copy()
+            self.mu = array_input[:,3].copy()
             self.initial_conditions_eta = array_input[:,4].copy()
 
+    def digest_statistics(self, ensemble_axis = 0, eta_axis = 0, verbose = False) -> np.array:
 
-        assert self.free_parameter_size == self.initial_conditions_eta.shape[0]
-        assert self._lambda_1_range == self._lambda_1.shape[0]
+        #FULL-WITH OF THE LAYER
+        #Collapse over ensemble axis, new shape (eta, lambda_1)
+        full_width_vector_per_initial_condition = np.max(self.max_width_matrix, axis=ensemble_axis)\
+            - np.min(self.min_width_matrix, axis=ensemble_axis)
+        _to_aux_file = np.zeros((self._lambda_1_range, 8))
+
+
+        full_width = np.min(full_width_vector_per_initial_condition, axis=0)
+        index_1 = np.where(full_width_vector_per_initial_condition == full_width)
+        half_width = full_width/2.
+        mLCE_vec = self.output_mLCE_matrix[index_1[0],:,index_1[1]]
+        omega_2 = self.initial_conditions_omega_2[0] * np.ones((self._lambda_1_range), dtype=_wp)
+
+        _to_aux_file[:,] = np.column_stack([self._lambda_1, self._lambda_2, omega_2,\
+                                            self.mu, self.initial_conditions_eta, self.upsilon,\
+                                            half_width, mLCE_vec])
+        np.savetxt(self.file_output_name, _to_aux_file)
+
+        headers = ["lambda_1", "lambda_2", "omega_2", "mu", "tilde_eta", "upsilon", "y_hw", "mlce"]
+        if verbose:
+            for values in _to_aux_file:
+                outstring = ""
+                for heads, vals in zip(headers, values):
+                    outstring = f"{outstring}{heads}: {vals} "
+                print(outstring)
 
 
 if __name__ == '__main__':
 
-    map_aguments = {'iteration_time' : 10**7,
+    map_aguments = {'iteration_time' : 10**4,
                     'initial_condition_size' : 256,
                     'free_parameter_size' : 1,
                     'omega_2_size' : 1,
@@ -39,11 +63,12 @@ if __name__ == '__main__':
                     'omega_2_initial_condition' : _wp(np.sqrt(2.5)),
                     'gen_whisker_map' : False,
                     'explicit_eta' : None,
+                    'pre_catched_eta' : True
                     }
     opencl_arguments_structure = {'global_size' : (map_aguments['initial_condition_size'],
-                                                   map_aguments['free_parameter_size'],
+                                                   1,
                                                    map_aguments['lambda_1_size']),
-                                  'local_size' : (16,1,16)}
+                                  'local_size' : (16,1,4)}
     date = time.strftime('%d-%m-%Y__%H:%M:%S')
     print(f"Start time: {date}")
     STATUS = f"data/wm_eta_found_{date}_gwm_{map_aguments['gen_whisker_map']}_it_time_\
@@ -53,12 +78,13 @@ if __name__ == '__main__':
 
 
     input_file = "./data/wm_eta_found_07-03-2026__21:08:00_gwm_False_it_time_100_eta_size_40_ensemble_size_256.dat"
-    Experiment_execution_instance = Experiment_execution_using_file(STATUS, map_aguments, input_file)
-    Experiment_execution_instance.set_program_script('./kernel_lambda_1_form.cl')
-
+    Experiment_execution_instance = Experiment_execution_using_file(STATUS, map_aguments)
+    Experiment_execution_instance.set_program_script('src/kernel_lambda_1_form.cl')
+    Experiment_execution_instance.set_file_as_initial_conditions(input_file)
     start_time = time.time()
+    Experiment_execution_instance.create_device_buffers()
     Experiment_execution_instance.execute_experiment(opencl_arguments_structure)
-    Experiment_execution_instance.digest_statistics()
+    Experiment_execution_instance.digest_statistics(verbose=True)
     end_time = (time.time() - start_time)/3600
     print("Time elapsed: ", end_time)
     Experiment_execution_instance.save_raw_data()
--- wm_eta_finder.py
+++ wm_eta_finder.py
@@ -13,10 +13,10 @@ _pi = 4.0*np.arctan(1.0) # System definition of pi
 np.random.seed(_random_seed)
 
 class Evolution_eta_finder:
-    _ONE_ETA_FLAG = _wpi(1)
-    _GWM_FLAG = _wpi(0)
-    _EXPLICIT_ETA = _wpi(1)
     def __init__(self,  arguments : dict):
+        self._ONE_ETA_FLAG = _wpi(0)
+        self._GWM_FLAG = _wpi(0)
+        self._EXPLICIT_ETA = _wpi(1)
         self._max_iter = arguments['iteration_time']
         self._dim_ensemble = arguments['initial_condition_size']
         self._dim_eta = arguments['free_parameter_size']
@@ -28,6 +28,7 @@ class Evolution_eta_finder:
         self._omega_2_ini = arguments['omega_2_initial_condition']
         self.is_gwm = arguments['gen_whisker_map']
         self.explicit_eta = arguments['explicit_eta']
+        self.pre_catched_eta = arguments['pre_catched_eta']
         self.__FLAGS()
         self._lambda_1 = self.set_lambda_1_initial_conditions()
         self.initial_conditions = self.set_initial_condition_xty()
@@ -40,8 +41,9 @@ class Evolution_eta_finder:
     def __FLAGS(self):
         if self.is_gwm:
             self._GWM_FLAG = _wpi(1)
+        if self.pre_catched_eta:
+            self._ONE_ETA_FLAG = _wpi(1)
         if self.explicit_eta is None:
-            self._ONE_ETA_FLAG = _wpi(0)
             self._EXPLICIT_ETA = _wpi(0)
 
     def set_lambda_1_initial_conditions(self) -> np.array:
@@ -103,9 +105,8 @@ class Experiment_execution(Evolution_eta_finder):
 
         self.min_width_matrix = np.zeros((self._dim_ensemble,\
                                           self._dim_eta, self._lambda_1_range), dtype=_wp)
-        self.__create_device_buffers()
 
-    def __create_device_buffers(self):
+    def create_device_buffers(self):
 
         self.OCL_Object.buffer_global(self.initial_conditions, "initial_conditions", False)
         self.OCL_Object.buffer_global(self.initial_conditions_eta, "initial_conditions_eta", False)
@@ -118,14 +119,14 @@ class Experiment_execution(Evolution_eta_finder):
         self.OCL_Object.buffer_global(self.min_width_matrix, "min_width_matrix")
         self.OCL_Object.buffer_global(self.mu, "mu")
 
-    def set_program_script(self, program_form : str = 'kernel_lambda_1_form.cl',\
-                           inculuded =['kernel_lambda_1.cl',\
+    def set_program_script(self, program_form : str = 'src/kernel_lambda_1_form.cl',\
+                           inculuded =['src/kernel_lambda_1.cl',\
                                        'src/jacobian.cl', 'src/modulus.cl']) -> None:
 
         with open(program_form, 'r') as file_to_change:
             script = file_to_change.read()
             script = script.replace("#define MAXITER", f"#define MAXITER {self._max_iter}")
-        with open('kernel_lambda_1.cl', 'w') as file:
+        with open('src/kernel_lambda_1.cl', 'w') as file:
             file.write(script)
         self.OCL_Object.program(inculuded, ['-I ./includes'])
 
@@ -172,13 +173,11 @@ class Experiment_execution(Evolution_eta_finder):
             mask : array of bool
         """
         mask = False
-        print(array_in.shape)
-        print(array_comp[0].shape)
         mask = array_in == array_comp
 
         return mask
 
-    def digest_statistics(self, ensemble_axis = 0, eta_axis = 0) -> np.array:
+    def digest_statistics(self, ensemble_axis = 0, eta_axis = 0, verbose = False) -> np.array:
 
         #FULL-WITH OF THE LAYER
         #Collapse over ensemble axis, new shape (eta, lambda_1)
@@ -198,9 +197,10 @@ class Experiment_execution(Evolution_eta_finder):
             lambda_1_el = self._lambda_1[ind]
             omega_2 = self.initial_conditions_omega_2[0]
             v = self.upsilon[ind]
-            print(f"Lambda_1:{lambda_1_el}  lambda_2: {lambda_2}  omega_2: {omega_2}")
-            print(f"mLCE:{mLCE}  half: {half_width}  c: {c}  v: {v}")
             _to_aux_file[ind, :] = np.array([lambda_1_el, lambda_2, omega_2,  mu_val,c, v, half_width])
+            if verbose:
+                print(f"Lambda_1:{lambda_1_el}  lambda_2: {lambda_2}  omega_2: {omega_2}")
+                print(f"mLCE:{mLCE}  half: {half_width}  c: {c}  v: {v}")
         np.savetxt(self.file_output_name, _to_aux_file)
 
     def save_raw_data(self, save : bool = False):
@@ -236,7 +236,7 @@ class Experiment_execution(Evolution_eta_finder):
         #
 
 if __name__ == '__main__':
-    map_aguments = {'iteration_time' : 10**2,
+    map_aguments = {'iteration_time' : 10**4,
                     'initial_condition_size' : 256,
                     'free_parameter_size' : 40,
                     'omega_2_size' : 1,
@@ -247,6 +247,7 @@ if __name__ == '__main__':
                     'omega_2_initial_condition' : _wp(np.sqrt(2.5)),
                     'gen_whisker_map' : False,
                     'explicit_eta' : None,
+                    'pre_catched_eta' : False,
                     }
     opencl_arguments_structure = {'global_size' : (map_aguments['initial_condition_size'],
                                                    map_aguments['free_parameter_size'],
@@ -260,8 +261,9 @@ if __name__ == '__main__':
 {map_aguments['initial_condition_size']}.dat"
 
     Experiment_execution_instance = Experiment_execution(STATUS, arguments_of_the_map=map_aguments)
-    Experiment_execution_instance.set_program_script('./kernel_lambda_1_form.cl')
+    Experiment_execution_instance.set_program_script('src/kernel_lambda_1_form.cl')
     start_time = time.time()
+    Experiment_execution_instance.create_device_buffers()
     Experiment_execution_instance.execute_experiment(opencl_arguments_structure)
     Experiment_execution_instance.digest_statistics()
     end_time = (time.time() - start_time)/3600
